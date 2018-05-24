75b3bd164ed6aa2b352c8dcda935aa3308571335
bigrob21 <bigrob21@homtail.com>
2018-05-23 20:09:55 -0400

    new file.

--- Chapter5/src/ex5/GoArrayAppend1.go
+++ Chapter5/src/ex5/GoArrayAppend1.go
@@ -7,4 +7,11 @@ func main(){
 	fmt.Println(x)
 	xx := append(x,100)
 	fmt.Println(xx)
+	
+	//Now this will be interesting
+	y := []int{1,2}
+	fmt.Println(" And the size of y is " + string(len(y)))
+//	yy := append(y,3,4,5)
+//	fmt.Println(yy)
+	
 }
